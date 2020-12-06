180feb7bfebe547ad539cdb62f4866e1750475cf
summit <sftth@naver.com>
2020-12-06 16:40:12 +0900

    jar 자동 실행 추가

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,34 @@ PROJECT_NAME=whms
 echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl whms | grep jar | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if[ -z "$CURRENT_PID"]; then
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 2
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup jar -jar \
+-Dspring.config.location=classpath:/application.properties,classpath:/application-dev.properties,/home/ec2-user/resources/application-oauth.properties,/home/ec2-user/resources/application-real-db.properties \
+-Dspring.profiles.active=dev \
+$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
