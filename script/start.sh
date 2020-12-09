19569890c0b38be210636115972436bdefacdb00
summit <sftth@naver.com>
2020-12-10 01:04:44 +0900

    무중단 배포 반영 2차 start.sh REPOSITORY 오타 수정

--- script/start.sh
+++ script/start.sh
@@ -4,12 +4,14 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITIORY=/home/ec2-user/app/step3
+REPOSITORY=/home/ec2-user/app/step3
 
 PROJECT_NAME=whms
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITIORY/zip/*.jar $REPOSITIORY"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
@@ -21,6 +23,10 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
+
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "> IDLE_PROFILE: $IDLE_PROFILE "
 
 nohup java -jar \
 -Dspring.config.location=classpath:/application.properties,classpath:/application-dev.properties,/home/ec2-user/resources/application-oauth.properties,/home/ec2-user/resources/application-real-db.properties \
